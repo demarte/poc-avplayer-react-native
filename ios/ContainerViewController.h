52731366a768acec5669353df88d6082b53a142a
Ivan <ivan@iMac.local>
2019-09-14 16:10:52 -0300

    Spinner AvPlayer

--- ios/ContainerViewController.h
+++ ios/ContainerViewController.h
@@ -16,6 +16,7 @@ NS_ASSUME_NONNULL_BEGIN
 @property(nonatomic, strong) AVPlayerViewController *playerViewController;
 @property (nonatomic, strong) AVPlayer *player;
 @property (nonatomic, strong) NSURL *videoURL;
+@property (nonatomic, strong) UIActivityIndicatorView *spinner;
 
 @end
 
--- ios/ContainerViewController.m
+++ ios/ContainerViewController.m
@@ -20,41 +20,97 @@
   self.view.backgroundColor = UIColor.redColor;
   
   [self configurePlayerViewController];
+  [self configureSpinner];
 }
 
 - (void)viewDidDisappear:(BOOL)animated {
   [super viewDidDisappear:animated];
   
   [NSNotificationCenter.defaultCenter removeObserver:self name:AVPlayerItemDidPlayToEndTimeNotification object:_player.currentItem];
+  
+  [_player removeObserver:self forKeyPath:@"playbackLikelyToKeepUp"];
+  [_player removeObserver:self forKeyPath:@"playbackBufferEmpty"];
 }
 
 -(void)configurePlayerViewController {
-  self.player = [AVPlayer playerWithURL: _videoURL];
-  self.playerViewController = [AVPlayerViewController new];
-  self.playerViewController.player = _player;
+  _player = [AVPlayer playerWithURL: _videoURL];
+  _playerViewController = [AVPlayerViewController new];
+  _playerViewController.player = _player;
+  _playerViewController.showsPlaybackControls = false;
   [self.view addSubview:_playerViewController.view];
   
-  [_player addObserver:self
-            forKeyPath:@"status"
-               options:(NSKeyValueObservingOptionNew)
-               context:nil];
+//  [_player addObserver:self
+//            forKeyPath:@"status"
+//               options:(NSKeyValueObservingOptionNew)
+//               context:nil];
+  
+  [_player.currentItem addObserver:self forKeyPath:@"playbackBufferEmpty" options:NSKeyValueObservingOptionNew context:nil];
+  [_player.currentItem addObserver:self forKeyPath:@"playbackLikelyToKeepUp" options:NSKeyValueObservingOptionNew context:nil];
+  [_player.currentItem addObserver:self forKeyPath:@"playbackBufferFull" options:NSKeyValueObservingOptionNew context:nil];
+  
+}
+
+-(void) configureSpinner {
+  _spinner = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
+  [_spinner startAnimating];
+  [self.view addSubview:_spinner];
+  [_spinner setTranslatesAutoresizingMaskIntoConstraints:NO];
+  
+  // CenterX constraint
+  [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.view
+                                                        attribute:NSLayoutAttributeCenterX
+                                                        relatedBy:NSLayoutRelationEqual
+                                                           toItem:_spinner
+                                                        attribute: NSLayoutAttributeCenterX
+                                                       multiplier:1
+                                                         constant:0]];
+  // CenterY constraint
+  [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.view
+                                                        attribute:NSLayoutAttributeCenterY
+                                                        relatedBy:NSLayoutRelationEqual
+                                                           toItem:_spinner
+                                                        attribute: NSLayoutAttributeCenterY
+                                                       multiplier:1
+                                                         constant:0]];
+ 
 }
 
 - (void)observeValueForKeyPath:(NSString *)keyPath
                       ofObject:(id)object
                         change:(NSDictionary *)change
-                       context:(void *)context
-{
-  if (object == _player && [keyPath isEqualToString:@"status"]) {
-    if (_player.status == AVPlayerStatusReadyToPlay) {
-      NSLog(@"PLAYING...");
-      
-//      [_player removeObserver:self forKeyPath:@"status"];
-      
-      NSLog(@"Remove obeservable");
-      
-//      [NSNotificationCenter.defaultCenter addObserver:self selector:@selector(didReachEnd) name:AVPlayerItemDidPlayToEndTimeNotification object: _player.currentItem];
+                       context:(void *)context {
+  
+  //  if (object == _player && [keyPath isEqualToString:@"status"]) {
+  //
+  //    if (_player.status == AVPlayerStatusReadyToPlay) {
+  //      NSLog(@"PLAYING...");
+  //
+  ////      [_player removeObserver:self forKeyPath:@"status"];
+  //
+  //      NSLog(@"Remove obeservable");
+  //    }
+  //  }
+  
+//  if (object == _player) {
+//    if ([keyPath isEqualToString: @"status"]) {
+//      if (_player.status == AVPlayerStatusReadyToPlay) {
+//        NSLog(@"PLAYING...");
+//      }
+//    }
+//  }
+  if (object == _player.currentItem) {
+    if ([keyPath isEqualToString: @"playbackBufferEmpty"]) {
+      NSLog(@"Buffer Empty...");
     }
+    if ([keyPath isEqualToString: @"playbackLikelyToKeepUp"]) {
+      NSLog(@"Keep UP...");
+      _playerViewController.showsPlaybackControls = true;
+      [_spinner stopAnimating];
+    }
+//    if ([keyPath isEqualToString: @"playbackBufferFull"]) {
+//      NSLog(@"Full...");
+//      _playerViewController.showsPlaybackControls = true;
+//    }
   }
 }
 
